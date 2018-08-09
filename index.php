3672ca7c73d989f534ed244c65a8613e4e075502
Unknown <joshhaz2012@gmail.com>
2018-08-09 13:15:20 +0100

    Basic content added to index page

--- index.php
+++ index.php
@@ -3,5 +3,21 @@ require './header.php';
 require './navbar.php';
 ?>
 <div class="container">
-    <h4>Welcome to Bioliner.</h4>
+    <br>
+    <div class="row">
+        <div class="col"></div>
+        <div class="col-10">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Welcome to Bioliner</h4>
+                    <div class="card-text">
+                        <p>Bioliner is a tool used to facilitate a 'pipeline' of bioinformatics jobs.</p>
+                        <p>This website can be used to create an input file for a workflow, or add a new module to Bioliner.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+
 </div>
