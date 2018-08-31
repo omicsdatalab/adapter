ebb28feb604e8fc0792e5330f58f0b9441bbcf49
Unknown <joshhaz2012@gmail.com>
2018-08-31 18:44:13 +0100

    Added guides page

---
+++ Guides.php
@@ -0,0 +1,28 @@
+<?php
+require 'header.php';
+require 'navbar.php';
+?>
+    <div class="container">
+        <div class="card-deck pad-top-3 lg-w-50 md-w-50 mx-auto">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">Users</h5>
+                    <p class="card-text">Instructions on how to use Bioliner and the website.</p>
+                </div>
+                <div class="card-footer">
+                    <a href="https://www.youtube.com/watch?v=jIdbW148Nd0" class="btn btn-secondary btn-block" target="_blank">Go To Video</a>
+                </div>
+            </div>
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">Developers</h5>
+                    <p class="card-text">Information explaining the Bioliner source code to future developers.</p>
+                </div>
+                <div class="card-footer">
+                    <a href="https://www.youtube.com/watch?v=9dqi8Tviddw" class="btn btn-secondary btn-block" target="_blank">Go To Video</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- navbar.php
+++ navbar.php
@@ -15,8 +15,14 @@
                 <li class="nav-item">
                     <a class="nav-link" href="./Downloads.php">Downloads</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./Guides.php">Guides</a>
+                </li>
             </ul>
             <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.php">OmicsDataLab</a>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="https://github.com/omicsdatalab/bioliner"><i class="fab fa-github"></i>GitHub</a>
                 </li>
